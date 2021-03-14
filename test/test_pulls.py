3c9bab8c7102ad4f3069397912341312a468e625
Maksimelyan Tamashevich <mtamashevich@gmail.com>
2021-03-14 13:37:02 -0400

    fix errors

--- test/test_pulls.py
+++ test/test_pulls.py
@@ -6,19 +6,18 @@ class TestPrime(TestCase):
 
     def setUp(self):
         """Init"""
-    
 
     def test_status(self):
 
         list1 = {'num': 15,
-                 'title': 'Homework1: Maksimelyan Tamashevich',
-                 'link': 'https://github.com/alenaPy/devops_lab/pull/15'
-                }
+            'title': 'Homework1: Maksimelyan Tamashevich',
+            'link': 'https://github.com/alenaPy/devops_lab/pull/15'
+        }
 
         list2 = {'num': 1,
-                 'title': 'Test PR', 
-                 'link': 'https://github.com/alenaPy/devops_lab/pull/1'
-                }
+            'title': 'Test PR', 
+            'link': 'https://github.com/alenaPy/devops_lab/pull/1'
+        }
         self.assertEqual(pulls.get_pulls('open').count(list1), 1)
         self.assertNotEqual(pulls.get_pulls('closed').count(list1), 1)
         self.assertEqual(pulls.get_pulls('closed').count(list2), 1)
